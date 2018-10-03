7225d0dbd5672e095cdb802db484e981d0a17a09
Nika Layzell <nika@thelayzells.com>
2018-10-03 12:07:42 -0400

    Add some helper scripts

---
+++ bump_version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version=$1
+old_version=`sed -n 's/^version = "\(.\+\)"/\1/p' cpp/Cargo.toml`
+
+echo "updating $old_version to $version"
+
+for toml in */Cargo.toml; do
+  cp $toml $toml.bk
+  cat $toml.bk \
+    | sed -e 's/\(\(^\|cpp_common.\+\)version = \"=\?\)'$old_version'/\1'$version'/g' \
+    > $toml
+  rm $toml.bk
+done
+
---
+++ publish_crates.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cargo publish --manifest-path cpp_common/Cargo.toml
+cargo publish --manifest-path cpp_build/Cargo.toml
+cargo publish --manifest-path cpp_macros/Cargo.toml
+cargo publish --manifest-path cpp/Cargo.toml
+
